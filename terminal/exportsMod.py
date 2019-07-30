902c428c2b42cf7f4fd4c58fcdd53336e0c561e3
Jatib <jatib@ciencias.unam.mx>
2019-07-30 11:58:03 -0500

    Creating package on terminal space

--- terminal/exportsMod.py
+++ terminal/exportsMod.py
@@ -1,60 +1,6 @@
 import os
 import shutil
 
-file = open("/home/developer/exports","r")
-
-def trunk(a):
-    data = a.readlines()
-    head = []
-    imod = 0
-    for i in range(len(data)):
-        if i-imod < len(data)-1:
-            if data[i-imod][0] == "#":
-                head.append(data[i-imod].split("\n")[0])
-                data.pop(i-imod)
-                imod += 1
-    return [data,head]
-
-def structureData(a):
-    structured_data = []
-    for line in a:
-        structure = []
-        if line[0] != '"':
-            for oDat in line.split(" "):
-                for dat in oDat.split("\t"):
-                    if dat != "":
-                        structure.append(dat.split("\n")[0])
-        else:
-            structure.append(line.split('"')[1])
-            for oDat in line.split('"')[2].split(" "):
-                for dat in oDat.split("\t"):
-                    if dat != "":
-                        structure.append(dat.split("\n")[0])
-        structured_data.append(structure)
-    return structured_data
-
-def ipData(a):
-    iPs = []
-    for line in a:
-        for ip in line[1:]:
-            dirs = []
-            purgedIp = ip.split("(")[0]
-            if purgedIp not in iPs and "." in purgedIp and "/" not in purgedIp:
-                dirs.append(purgedIp)
-                for lin in a:
-                    if ip in lin:
-                        dirs.append(lin[0])
-                iPs.append(dirs)
-    return iPs
-
-def load():
-    file = open("/home/developer/exports","r")
-    data = trunkHead(file)
-    structured = structureData(data[0])
-    iPs = ipData(structured)
-
-    return data,structured,iPs
-
 data = trunk(file)
 structured = structureData(data[0])
 iPs = ipData(structured)
