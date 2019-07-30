2c1c23f9450373cf4734a98074461c07febe9b72
Jatib <jatib@ciencias.unam.mx>
2019-07-30 08:33:51 -0500

    Start of project, first update of data

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # NFSManager
+
+This is an implentation of software to manage an NFS system, by terminal, and by GUI. Still under deployment.
---
+++ terminal/exportsManager/Manager.py
@@ -0,0 +1,6 @@
+class exportsManager(object):
+    """docstring for exportsManager."""
+
+    def __init__(self, arg):
+        super(exportsManager, self).__init__()
+        self.arg = arg
