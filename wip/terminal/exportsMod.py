9abc6efcb296a1b1a7cec61df39d459dc30f64aa
jatib <quantum.jatib@gmail.com>
2019-08-02 16:47:20 -0500

    Modificación del menú de interacción principal
    Se modifica la UI para pasar al equipo de UI y UX

--- wip/terminal/exportsMod.py
+++ wip/terminal/exportsMod.py
@@ -1,9 +1,9 @@
 import os
 import shutil
 
-data = trunk(file)
-structured = structureData(data[0])
-iPs = ipData(structured)
+#data = trunk(file)
+#structured = structureData(data[0])
+#iPs = ipData(structured)
 
 def flujo(fin=False):
     if fin == False:
@@ -14,11 +14,15 @@ def flujo(fin=False):
             print("Selecciona una de las siguientes opciones:\n1)Filtrar\n2)Mostrar todo\n\n")
             option = int(input("Introduzca número de opción:\n"))
             print("\n\n")
-            if option == 1:
-                query = input("Introduzca una nueva ip:\n")
-                if query in iPs:
-                    print(iPs.index(query))
-                else:
-                    print("La ip señalada no existe")
-        flujo()
+        elif option == 1:
+            query = input("Introduzca una nueva ip:\n")
+                #if query in iPs:
+                #    print(iPs.index(query))
+                #else:
+                #    print("La ip señalada no existe")
+        elif option == 0:
+            flujo(True)
+        else:
+            print("No reconozco el comando")
+            flujo()
 flujo()
